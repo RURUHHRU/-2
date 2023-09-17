75902e805b78b8f73ac997c779e8b8cb483e28ae
RURUHHRU <106974939+RURUHHRU@users.noreply.github.com>
2023-09-17 21:40:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+
+namespace 药店管理
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseDefaultFiles(); // 添加默认文件支持
+            app.UseStaticFiles(); // 启用静态文件托管
+           
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
